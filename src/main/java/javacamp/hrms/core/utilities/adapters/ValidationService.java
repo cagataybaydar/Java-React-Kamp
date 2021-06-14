e6cf485293f4c9aab0774b7063b66a99bd955ffa
Çağatay <83411107+cagataybaydar@users.noreply.github.com>
2021-06-14 22:36:41 +0300

    Delete src directory

--- src/main/java/javacamp/hrms/core/utilities/adapters/ValidationService.java
+++
@@ -1,5 +0,0 @@
-package javacamp.hrms.core.utilities.adapters;
-
-public interface ValidationService {
-	boolean validateByMernis(long nationality_Id, String first_name, String last_name, int birthYear);
-}
