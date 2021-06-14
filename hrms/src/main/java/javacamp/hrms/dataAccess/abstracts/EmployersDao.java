62e7d88278694e9826cb7951a27e55b13e756322
Çağatay <83411107+cagataybaydar@users.noreply.github.com>
2021-06-14 21:04:07 +0300

    Delete hrms directory

--- hrms/src/main/java/javacamp/hrms/dataAccess/abstracts/EmployersDao.java
+++
@@ -1,9 +0,0 @@
-package javacamp.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import javacamp.hrms.entities.concretes.Employers;
-
-public interface EmployersDao extends JpaRepository<Employers, Integer> {
-
-}
--- hrms/src/main/java/javacamp/hrms/dataAccess/abstracts/JobTitlesDao.java
+++
@@ -1,9 +0,0 @@
-package javacamp.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import javacamp.hrms.entities.concretes.JobTitles;
-
-public interface JobTitlesDao extends JpaRepository<JobTitles, Integer> {
-	
-}
